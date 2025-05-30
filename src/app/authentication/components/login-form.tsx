25581b9e69efcaeb413337d119946511cec6ac55
Debora Souza <debora.souzalima22@gmail.com>
2025-05-30 18:52:58 -0300

    feat: add login

--- src/app/authentication/components/login-form.tsx
+++ src/app/authentication/components/login-form.tsx
@@ -2,11 +2,21 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import z from "zod";
 
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 // Imports do Form
 import {
   Form,
@@ -15,79 +25,108 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-
+import { authClient } from "@/lib/auth/auth-client";
 const loginSchema = z.object({
-    email: z.string().trim().email({ message: "Email inválido" }),
-    password: z.string().trim().min(8, { message: "Senha deve ter pelo menos 8 caracteres" }),
-  })
-  
+  email: z.string().trim().email({ message: "Email inválido" }),
+  password: z
+    .string()
+    .trim()
+    .min(8, { message: "Senha deve ter pelo menos 8 caracteres" }),
+});
+
 const LoginForm = () => {
-    // Define your form.
-    const form = useForm<z.infer<typeof loginSchema>>({
-        resolver: zodResolver(loginSchema),
-        defaultValues: {
-        email: "",
-        password: "",
+  const router = useRouter();
+  // Define your form.
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  // Define a submit handler.
+  async function onSubmit(values: z.infer<typeof loginSchema>) {
+    await authClient.signIn.email(
+      {
+        email: values.email,
+        password: values.password,
+      },
+      {
+        onSuccess: () => {
+          router.push("/dashboard");
         },
-    })
+        onError: () => {
+          toast.error("E-mail ou Senha inválidos.");
+        },
+      },
+    );
+  }
 
-    // Define a submit handler.
-        function onSubmit(values: z.infer<typeof loginSchema>) {
-        console.log(values)
-    }
-
-    return (
-        <div>
-        <Card>
-          {/* Form Register */}
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-          <CardHeader>
-            <CardTitle>Login</CardTitle>
-            <CardDescription>
-              Faça login para continuar
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-4">
-          <FormField
+  return (
+    <div>
+      <Card>
+        {/* Form Register */}
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <CardHeader>
+              <CardTitle>Login</CardTitle>
+              <CardDescription>Faça login para continuar</CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-4">
+              <FormField
                 control={form.control}
                 name="email"
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Email</FormLabel>
                     <FormControl>
-                        <Input placeholder="Digite seu email" {...field} />
+                      <Input placeholder="Digite seu email" {...field} />
                     </FormControl>
 
                     <FormMessage />
                   </FormItem>
                 )}
-          />
-          <FormField
+              />
+              <FormField
                 control={form.control}
                 name="password"
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Senha</FormLabel>
                     <FormControl>
-                        <Input placeholder="Digite sua senha" type="password" {...field} />
+                      <Input
+                        placeholder="Digite sua senha"
+                        type="password"
+                        {...field}
+                      />
                     </FormControl>
 
                     <FormMessage />
                   </FormItem>
                 )}
-          />
-          </CardContent>
-          <CardFooter>
-          <Button type="submit" className="w-full">Entrar</Button>
-          </CardFooter>
+              />
+            </CardContent>
+            <CardFooter>
+              <Button
+                type="submit"
+                className="w-full"
+                disabled={form.formState.isSubmitting}
+              >
+                {form.formState.isSubmitting ? (
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                ) : (
+                  "Entrar"
+                )}
+              </Button>
+            </CardFooter>
           </form>
-          </Form>
-        </Card>
-        </div>
-    )
-}
+        </Form>
+      </Card>
+    </div>
+  );
+};
 
 export default LoginForm;
