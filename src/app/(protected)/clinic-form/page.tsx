1401f1e193c6a30d7fe2e5adea105850ba0cbd2f
Debora Souza <debora.souzalima22@gmail.com>
2025-09-21 22:15:08 -0300

    fix: update clinic form dialog description for clarity

--- src/app/(protected)/clinic-form/page.tsx
+++ src/app/(protected)/clinic-form/page.tsx
@@ -29,7 +29,7 @@ const ClinicFormPage = async () => {
           <DialogHeader>
             <DialogTitle>Adicionar Clínica</DialogTitle>
             <DialogDescription>
-              Adicione uma Clínica para continuar.
+              Adicione uma Clínica para continuar navegando..
             </DialogDescription>
           </DialogHeader>
           <ClinicForm />
